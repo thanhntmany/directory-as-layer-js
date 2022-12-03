4910036e847985a5e1dc899e1dc5f1c1d6f8a759
Thanhntmany <thanhntmany@thanhntmany-laptop>
2022-12-03 18:03:43 +0700

    combine DALStack in to DALHandler

--- core/dal.js
+++ core/dal.js
@@ -1,20 +1,59 @@
 'use strict';
-const DALStack = require('./dal-stack');
+const { isString } = require('./helper/string-helper');
+const DALLayer = require('./dal-layer');
 
 
 // @@ Main class
 const Class = function DALHandler() {
-  this.stack = DALStack.create();
   this.exclude = [];
+  this.stack = [];
+  this._key = {};
 };
 
 const _proto = Class.prototype;
 
 
+// @ Modify stack
+_proto.insert = function (...layerPayloads) {
+  this.stack.push.apply(this.stack, DALLayer.massInit(layerPayloads));
+  return this;
+};
+
+_proto.insertAt = function (index, ...layerPayloads) {
+  this.stack.splice.apply(this.stack, [index, 0].concat(DALLayer.massInit(layerPayloads)));
+  return this;
+};
+
+_proto.splice = function (start, deleteCount, ...layerPayloads) {
+  return this.stack.splice.apply(this.stack, [start, deleteCount].concat(DALLayer.massInit(layerPayloads)));
+};
+
+
 // @@ class's functions
+_proto.refresh_key = function () {
+
+  var _this = this;
+  this.stack.forEach(dalLayer => {
+    if (isString(dalLayer.key) && isNaN(dalLayer.key)) _this._key[dalLayer.key] = dalLayer;
+  });
+
+  // The 'base' key is always point to the first layer.
+  this._key['base'] = this.stack[0];
+};
+
+_proto.loadLayers = function (layersPayload) {
+  if (!Array.isArray(layersPayload)) layersPayload = [];
+
+  this.stack = DALLayer.massInit(layersPayload);
+  this.refresh_key();
+
+  return this;
+};
+
+
 _proto.loadDAL = function (payload) {
   if (Array.isArray(payload.exclude)) this.exclude = payload.exclude;
-  this.stack.init(payload.stack);
+  this.loadLayers(payload.stack);
   return this;
 };
 
